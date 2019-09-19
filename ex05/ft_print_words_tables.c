63c611e0a5624ee56827d0f3de0bf983d8c2c3e0
Sander Verheijen <sanderverheijen@outlook.com>
2019-09-19 09:51:38 +0200

    ex05

---
+++ ex05/ft_print_words_tables.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_print_words_tables.c                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: averheij <averheij@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/09/19 09:43:16 by averheij       #+#    #+#                */
+/*   Updated: 2019/09/19 09:49:47 by averheij      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	int count;
+
+	count = 0;
+	while (str[count] != '\0')
+	{
+		ft_putchar(str[count]);
+		count = count + 1;
+	}
+	ft_putchar('\n');
+}
+
+void	ft_print_words_tables(char **tab)
+{
+	int i;
+
+	i = 0;
+	while (tab[i] != 0)
+	{
+		ft_putstr(tab[i]);
+		i++;
+	}
+}
---
+++ ex05/main.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: averheij <averheij@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/09/19 09:43:50 by averheij       #+#    #+#                */
+/*   Updated: 2019/09/19 09:50:54 by averheij      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_print_words_tables(char **tab);
+
+char	**ft_split_whitespaces(char *str);
+
+int		main(void)
+{
+	char *str = "hello t are    sepertate 		cool kid 	words    	w";
+	char **res;
+
+	res = ft_split_whitespaces(str);
+	ft_print_words_tables(res);
+}
